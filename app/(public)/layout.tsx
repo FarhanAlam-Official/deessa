115d2d1fe50ee14d59f0c24a424059117d35aad2
Farhan Alam <115414440+FarhanAlam-Official@users.noreply.github.com>
2025-12-23 20:57:18 +0545

    style: Enhance layout structure and improve navbar responsiveness with updated styles

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -9,11 +9,13 @@ export default function PublicLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
+    <>
       <IntroVideo />
       <Navbar />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
+      <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </>
   )
 }
